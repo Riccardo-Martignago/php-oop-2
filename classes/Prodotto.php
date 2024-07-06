80dba5163903de36e377917540b168b4fe96ef55
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-07-06 17:24:15 +0200

    Aggiunto classi e dati prodotto

---
+++ classes/Prodotto.php
@@ -0,0 +1,28 @@
+<?php
+
+class Prodotto {
+    public $titolo;
+    public $categoria
+    public $foto;
+    public $prezzo;
+
+    public function __construct($titolo, $categoria, $foto, $prezzo){
+        $this->titolo = $titolo;
+        $this->categoria = $categoria;
+        $this->foto = $foto;
+        $this->prezzo = $prezzo;
+    }
+    
+    public function getTitolo() {
+        return $this->titolo;
+    }
+    public function getCategoria() {
+        return $this->categoria;
+    }
+    public function getFoto() {
+        return $this->foto;
+    }
+    public function getPrezzo() {
+        return $this->prezzo;
+    }
+}
